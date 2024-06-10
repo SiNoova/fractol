06b7e2471f2c74b5186fafb75601d368f1746b6e
Alae Koutate <akoutate@e1r7p12.1337.ma>
2024-06-10 11:01:35 +0100

    s

--- fractol.h
+++ fractol.h
@@ -6,7 +6,7 @@
 /*   By: akoutate <akoutate@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/20 17:56:11 by akoutate          #+#    #+#             */
-/*   Updated: 2024/05/04 11:07:41 by akoutate         ###   ########.fr       */
+/*   Updated: 2024/06/10 11:01:25 by akoutate         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -38,23 +38,18 @@ typedef struct s_data
 	t_comp	*z;
 	double	x_start;
 	double	x_end;
-	double y_start;
+	double	y_start;
 	double	y_end;
-	char 	*set;
+	char	*set;
 	double	imag;
 	double	real;
 }	t_data;
-
-typedef struct infos
-{
-
-}	t_infos;
 
 int		create_trgb(int t, int r, int g, int b);
 void	my_mlx_pixel_put(t_data *data, int x, int y, int color);
 void	fill_image(t_comp c, t_comp z, t_data *data);
 int		ft_strcmp(const char *s1, const char *s2);
 double	my_atoi(char *str);
-int ft_strlen(char *str);
+int		ft_strlen(char *str);
 
 #endif
